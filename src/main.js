3e051616b78d02973930dd76aca75b0589dd97b4
Thanh Le <lhthanh@gmail.com>
2022-11-17 21:02:06 +0700

    Create a contact listing page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from './App.vue'
+import router from './router';
+createApp(App).use(router).mount('#app');
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+
