56904c6f6e8f7ae9d465b1cab986e1a3c1a7247d
Thanh Le <lhthanh@gmail.com>
2022-11-17 21:21:02 +0700

    Create an edit page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,12 @@ import { createWebHistory, createRouter } from 'vue-router';
 import ContactBook from '@/views/ContactBook.vue';
 const routes = [
     {
+        path: '/contacts/:id',
+        name: 'contact.edit',
+        component: () => import('@/views/ContactEdit.vue'),
+        props: (route) => ({ contactId: parseInt(route.params.id) })
+        },
+    {
         path: '/:pathMatch(.*)*',
         name: 'notfound',
         component: () => import('@/views/NotFound.vue'),
--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -32,6 +32,13 @@
                     <i class="fas fa-address-card" />
                 </h4>
                 <ContactCard :contact="activeContact" />
+                <router-link :to="{
+                    name: 'contact.edit',
+                    params: { id: activeContact.id },
+                }">
+                    <span class="mt-2 badge badge-warning">
+                        <i class="fas fa-edit" /> Hiệu chỉnh</span>
+                </router-link>
             </div>
         </div>
     </div>
@@ -117,6 +124,8 @@ export default {
     mounted() {
         this.refreshList();
     },
+
+
 };
 </script>
 <style scoped>
